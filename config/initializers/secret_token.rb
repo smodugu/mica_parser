bb86d874d59d79c67a1661a95e05aea76abafd20
smodugu <surendar.modugu@machinima.com>
2012-09-08 10:11:15 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MicaParser::Application.config.secret_token = '3499b833f62008ed806348372818ee4a0193d9e234641710dc0ad06ee03c6b34372b405d67cdbdf84cd563f067a8a474c0724f486772ddf80d5a4ffd5241f84c'
